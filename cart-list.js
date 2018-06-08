9d231143c47ee5d2be38b3d245b3f95ffea3217b
Charity Cardinal <charcard@umich.edu>
2018-06-07 23:05:14 -0400

    Got functionality

--- cart-list.js
+++ cart-list.js
@@ -5,14 +5,14 @@ const pool = require("./pg-connection-pool");
 const cartList = express.Router();
 
 cartList.get("/cart-items", (req, res) => {
-  pool.query("SELECT * FROM ShoppingCart").then((result) => {
+  pool.query("SELECT * FROM ShoppingCart ORDER BY id").then((result) => {
     res.send(result.rows);
   });
 });
 
 cartList.post("/cart-items", (req, res) => {    
-  pool.query("INSERT INTO ShoppingCart(product, price, quantity) VALUES($1::text, $2::smallint, $3::smallint)", [request.body.product, request.body.price, request.body.quantity]).then(() => {
-      pool.query("SELECT * FROM ShoppingCart").then((result) => {
+  pool.query("INSERT INTO ShoppingCart (product, price, quantity) VALUES($1::text, $2::int, $3::int)", [req.body.product, req.body.price, req.body.quantity]).then(() => {
+      pool.query("SELECT * FROM ShoppingCart ORDER BY id").then((result) => {
           res.send(result.rows);
       });    
   });
@@ -27,11 +27,11 @@ cartList.delete("/cart-items/:id", (req, res) => {
 });
 
 cartList.put("/cart-items/:id", (req, res) => {
-  pool.query("UPDATE ShoppingCart SET product=$1::text, price=$2::smallint, quantity=$3::smallint, WHERE id=$4::int", [req.body.product, req.body.price, req.body.quantity]).then(() => {
-      pool.query("SELECT * FROM ShoppingCart ORDER BY id").then((result) => {
-          res.send(result.rows);
-      }); 
-  })
+  pool.query("UPDATE ShoppingCart SET product=$1::text, price=$2::smallint, quantity=$3::smallint WHERE id=$4::int", [req.body.product, req.body.price, req.body.quantity, req.params.id]).then(() => {
+    pool.query("SELECT * FROM ShoppingCart ORDER BY id").then((result) => {
+        res.send(result.rows);
+    });
+})
 });
 
 module.exports = cartList;
--- public/cart-items.js
+++ public/cart-items.js
@@ -3,39 +3,47 @@
 const cartItems = {
   template: `
   <div>
-    <section ng-repeat="item in $ctrl.cart-list">
-      <input type="text" ng-blur="$ctrl.updateItem(item);" ng-model="item.product">
-      <input type="text" ng-blur="$ctrl.updateItem(item);" ng-model="item.price">
-      <input type="text" ng-blur="$ctrl.updateItem(item);" ng-model="item.quantity">
+    <section ng-repeat="item in $ctrl.cartlist">
+
+    {{3+4}}
+      <input type="text" ng-blur="$ctrl.updateItem(item)" ng-model="item.product">
+      <input type="text" ng-blur="$ctrl.updateItem(item)" ng-model="item.price">
+      <input type="text" ng-blur="$ctrl.updateItem(item)" ng-model="item.quantity">
       <a href="" ng-click="$ctrl.deleteItem(item.id);">X</a>
     </section>
   </div>
   <form ng-submit="$ctrl.addItem($ctrl.newItem);">
+    {{item.product}}
     <input type="text" placeholder="Product" ng-model="$ctrl.newItem.product">
     <input type="text" placeholder="Price" ng-model="$ctrl.newItem.price">
     <input type="text" placeholder="Quantity" ng-model="$ctrl.newItem.quantity"> 
     <button>Submit</button>
   </form>
+
+
   `,
   controller: ["CartService", function(CartService) {
     const vm = this;
     CartService.getAllItems().then((response) => {
-      vm.cartList = response.data;
+      console.log(response.data);
+      vm.cartlist = response.data;
     });
     vm.addItem = (newItem) => {
       CartService.addItem(newItem).then((response) => {
-        vm.cartList = response.data;
+        vm.cartlist = response.data;
+        console.log(newItem);
       });
+      console.log(newItem);
       vm.newItem = {};
     }
     vm.deleteItem = (id) => {
       CartService.deleteItem(id).then((response) => {
-        vm.cartList = response.data;
+        vm.cartlist = response.data;
       });
     }
     vm.updateItem = (item) => {
       CartService.updateItem(item).then((response) => {
-        vm.cartList = response.data;
+        vm.cartlist = response.data;
       })
     }
   }] 
@@ -44,3 +52,50 @@ const cartItems = {
 angular
   .module("app")
   .component("cartItems", cartItems);
+
+// "use strict";
+
+// const cartItems = {
+//   template: `
+//   <div>
+//     <section ng-repeat="item in $ctrl.cart-list">
+//       <input type="text" ng-blur="$ctrl.updateItem(item);" ng-model="item.product">
+//       <input type="text" ng-blur="$ctrl.updateItem(item);" ng-model="item.price">
+//       <input type="text" ng-blur="$ctrl.updateItem(item);" ng-model="item.quantity">
+//       <a href="" ng-click="$ctrl.deleteItem(item.id);">X</a>
+//     </section>
+//   </div>
+//   <form ng-submit="$ctrl.addItem($ctrl.newItem);">
+//     <input type="text" placeholder="Product" ng-model="$ctrl.newItem.product">
+//     <input type="text" placeholder="Price" ng-model="$ctrl.newItem.price">
+//     <input type="text" placeholder="Quantity" ng-model="$ctrl.newItem.quantity"> 
+//     <button>Submit</button>
+//   </form>
+//   `,
+//   controller: ["CartService", function(CartService) {
+//     const vm = this;
+//     CartService.getAllItems().then((response) => {
+//       vm.cartlist = response.data;
+//     });
+//     vm.addItem = (newItem) => {
+//       CartService.addItem(newItem).then((response) => {
+//         vm.cartlist = response.data;
+//       });
+//       vm.newItem = {};
+//     }
+//     vm.deleteItem = (id) => {
+//       CartService.deleteItem(id).then((response) => {
+//         vm.cartlist = response.data;
+//       });
+//     }
+//     vm.updateItem = (item) => {
+//       CartService.updateItem(item).then((response) => {
+//         vm.cartlist = response.data;
+//       })
+//     }
+//   }] 
+// } 
+
+// angular
+//   .module("app")
+//   .component("cartItems", cartItems);
